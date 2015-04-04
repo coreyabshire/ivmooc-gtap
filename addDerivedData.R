a1afb02f65f3a9ee1a024d49372cb5ef558acd54
Ronny De Winter <ronny.de.winter@gmail.com>
2015-04-04 20:40:18 +0200

    add variables to trade derived from the base variables and save as trade.plus

---
+++ addDerivedData.R
@@ -0,0 +1,82 @@
+# Enrich the basic trade data with derivative data:
+#
+#  1.  Sum Export per Exporting Region per Year
+#  2.  Sum Total Export over full period per Region
+#  3.  Sum Import per Improting Region per Year
+#  4.  Sum Total Import over full period per Region
+#  5.  Sum total Trade for a commodity per Year
+#  6.  Sum total Trade per Commodity over full period
+#  7.  Create tradeplus dataset with added variables
+#       - market: market share for an exporting region for a commodity in a year
+#       - weigth: export of commodity vs full export of that region in that year
+#  8.  Top exported commmodities for a region
+
+library(dplyr)
+load("trade.clean")
+
+# 1.  Sum Export per Exporting Region per Year (all commodities)
+Sum_x_ExpYear <- trade %>%
+  group_by(exp, year) %>%
+  summarise(commval = sum(value)) %>%
+  arrange(commval)
+
+# 2.  Sum Total Export over full period per Region
+Sum_x_Exp <- Sum_x_ExpYear %>%
+  group_by(exp) %>%
+  summarise(commval = sum(commval)) %>%
+  arrange(desc(commval))
+
+# 3.  Sum Import per Importing Region per Year (all commodities)
+Sum_x_ImpYear <- trade %>%
+  group_by(imp, year) %>%
+  summarise(commval = sum(value)) %>%
+  arrange(commval)
+
+# 4.  Sum Total Import over full period per Region
+Sum_x_Imp <- Sum_x_ImpYear %>%
+  group_by(imp) %>%
+  summarise(commval = sum(commval)) %>%
+  arrange(desc(commval))
+
+# 5.  Sum total Trade for a commodity per Year
+Sum_x_CommYear <- trade %>%
+  group_by(comm, year) %>%
+  summarise(commval = sum(value)) %>%
+  arrange(commval)
+
+# 6.  Sum total Trade per Commodity over full period
+Sum_x_Comm <- Sum_x_CommYear %>%
+  group_by(comm) %>%
+  summarise(commval = sum(commval)) %>%
+  arrange(desc(commval))
+
+# 7.  Create tradeplus dataset with added variables
+#       - market: market share for an exporting region for a commodity in a year
+#       - weigth: export of commodity vs full export of that region in that year
+
+# add market
+tradeplus = left_join(trade, Sum_x_CommYear)
+tradeplus = rename(tradeplus, market = commval)
+tradeplus$market = tradeplus$value / tradeplus$market
+# add weight
+tradeplus = left_join(tradeplus, Sum_x_ExpYear)
+tradeplus = rename(tradeplus, weight = commval)
+tradeplus$weight = tradeplus$value / tradeplus$weight
+# Save to trade.plus
+save(tradeplus, file="trade.plus")
+
+# 8.  Top exported commodities for a region
+
+topTrades <- function(region, threshold = 0.05) {
+  t = tradeplus %>% 
+    filter(exp == region) %>%
+    group_by(comm) %>%
+    summarise(commval = sum(value)) %>%
+    arrange(desc(commval))
+  totExport = sum(t$commval)
+  t$weight = t$commval / totExport
+  t[t$weight > threshold,]
+}
+
+topTrades("pol")
+topTrades("bel", 0.02)
