91fcd5b9574172bfa46819cc9ebf1e3982621ce9
Ronny De Winter <ronny.de.winter@gmail.com>
2015-04-07 18:30:00 +0200

    replace market by export value for the line width (size)

--- CommTrade4Region.R
+++ CommTrade4Region.R
@@ -1,14 +1,14 @@
 library(ggplot2)
 library(dplyr)
 
-load(trade.plus)
+load("trade.plus")
 
 export4region <- function(region, comms = topTrades(region)) {
     reg = tradeplus %>% 
         filter(exp == region & comm %in% comms) %>% 
         group_by(exp,comm,year) %>% 
-        summarise(weight = sum(weight), market = sum(market))
-    qplot(year, weight, col=comm, size=market, data=reg) +
+        summarise(weight = sum(weight), value = sum(value))
+    qplot(year, weight, col=comm, size=value, data=reg) +
       geom_line() +
       ggtitle(paste("export for ", region))
 }
