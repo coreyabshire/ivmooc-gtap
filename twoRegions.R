3e41878c26cc63e2c97f8d99b0344b58df9aeb93
Ronny De Winter <ronny.de.winter@gmail.com>
2015-04-22 16:21:13 +0200

    for poster add comm descriptions + lorger fonts and other colors

--- twoRegions.R
+++ twoRegions.R
@@ -3,6 +3,7 @@ library(dplyr)
 
 load("trade.plus")
 
+tradeplus$value = tradeplus$value/1000
 ## @TODO top5 commodities for each region
 twoRegions <- function(region1, region2) {
   bilat = tradeplus %>% 
@@ -16,17 +17,48 @@ twoRegions <- function(region1, region2) {
 twoRegions("chn", "usa")
 
 # Export for 2 region2 and main commodities (example: China (chn) and usa)
+##########################################################################
 twoRegionsMainCommodities <- function(region1, region2, comms) {
+  trade$value = trade$value / 1000
   byExp <- trade %>%
     filter(exp == region1 | exp == region2) %>%
     filter(comm %in% comms) %>%
     group_by(comm, year, exp) %>%
-    summarise(commval = sum(value))
-  byExp$comm <- factor(byExp$comm, levels = comms) # order by top worldtraded commodities
-  qplot(year, commval, data = byExp, facets = . ~ comm, col = exp, geom = "line") + 
-    facet_wrap(~ comm, ncol=5) +
-    ggtitle(paste("Export of World Top Traded Commodities by", region1, "and", region2))
+    summarise(exported_value = sum(value)) %>% ungroup()
+
+  byExpLongComm = left_join(byExp, sectors, by = c("comm" = "Code"))
+  byExpLongComm$comm <- factor(byExpLongComm$comm, levels = comms) # order by top traded commodities
+  qplot(year, exported_value, data = byExpLongComm, facets = . ~ Description, 
+        col = exp, geom = "line", size=2) + 
+    scale_colour_manual(values = c("chn" = "red","usa" = "blue")) +
+    facet_wrap(~ Description, ncol=4) +
+    ggtitle(paste("Export Top Traded Commodities by", region1, "and", region2)) +
+    theme(plot.title=element_text(face="bold", size=25), 
+          axis.text = element_text(size=15),
+          axis.title = element_text(size=20),
+          legend.position="none")   
 }
 
-topComms = Sum_x_Comm[1:15,][[1]]
-twoRegionsMainCommodities("chn", "usa", topComms)
+ggplot(df3, aes(x=year)) +
+  geom_line(data = dfmin, aes(x=year, y=balance, col = exp, alpha = 0.1), size=2) +
+  geom_text(label = "China", x = 2005, y = 400, colour = "red", size = 10) +
+  geom_line(data=df3, aes(y=chn), col="red", size=3) +
+  geom_text(label = "USA", x = 1997, y = -500, colour = "blue", size = 10) +
+  geom_line(data=df3, aes(y=usa), col= "blue", size=3) +
+  ggtitle("Trading balance 30 biggest exporting countries") +
+  theme(plot.title=element_text(face="bold", size=25), 
+        axis.text = element_text(size=15),
+        axis.title = element_text(size=20),
+        legend.position="none")
+
+
+
+
+############################
+topcomms = trade %>% filter(year == 2009, exp == "chn" | exp == "usa") %>% 
+  group_by(comm) %>% 
+  summarize(val = sum(value)) %>% 
+  ungroup() %>% arrange(-val) 
+topcomms = topcomms[1:12,][[1]]
+twoRegionsMainCommodities("chn", "usa", topcomms)
+
