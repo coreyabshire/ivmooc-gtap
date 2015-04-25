d13b55c1d0450a2bc07d3922d5730ae43c5d9af2
Ronny De Winter <ronny.de.winter@gmail.com>
2015-04-25 21:26:50 +0200

    larger font size for poster, value/1000

--- CommTrade4Region.R
+++ CommTrade4Region.R
@@ -4,13 +4,18 @@ library(dplyr)
 load("trade.plus")
 
 export4region <- function(region, comms = topTrades(region)) {
+    tradeplus$value = tradeplus$value / 1000
     reg = tradeplus %>% 
         filter(exp == region & comm %in% comms) %>% 
         group_by(exp,comm,year) %>% 
         summarise(weight = sum(weight), value = sum(value))
     qplot(year, weight, col=comm, size=value, data=reg) +
       geom_line() +
-      ggtitle(paste("export for ", region))
+      ggtitle(paste("export for ", region)) +
+      theme(plot.title=element_text(face="bold", size=35), 
+            axis.text = element_text(size=25),
+            axis.title = element_text(size=25),
+            legend.text = element_text(size=15))
 }
 
 export4region("usa")
--- ExpPartners4Region.R
+++ ExpPartners4Region.R
@@ -4,13 +4,18 @@ library(dplyr)
 load("trade.plus")
 
 exportPartners4region <- function(region, partners = topPartners(region)) {
+  tradeplus$value = tradeplus$value / 1000
   reg = tradeplus %>% 
     filter(exp == region & imp %in% partners) %>% 
     group_by(exp,imp,year) %>% 
     summarise(weight = sum(weight), value = sum(value))
   qplot(year, weight, col=imp, size=value, data=reg) +
     geom_line() +
-    ggtitle(paste("export partners for ", region))
+    ggtitle(paste("export partners for", region)) +
+    theme(plot.title=element_text(face="bold", size=35), 
+          axis.text = element_text(size=25),
+          axis.title = element_text(size=25),
+          legend.text = element_text(size=15))
 }
 
 exportPartners4region("chn")
